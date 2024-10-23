42af04617c5d5fd0e91a6901090a617a2ce69c51
ZalewskiDamian <damian.github@gmail.com>
2024-10-23 22:58:22 +0200

    add jango skills

--- src/pages/marineford/jango.jsx
+++ src/pages/marineford/jango.jsx
@@ -15,6 +15,22 @@ import { Text } from "@/components/ui/text";
 
 import headingImage from "@/assets/images/heading/jango-heading.png";
 
+//SKILL RANGE
+import skill_stronger_range from "@/assets/images/marineford/jango/jango-stronger-range.png";
+import skill_kira_chakram_range from "@/assets/images/marineford/jango/jango-kira-chakram-range.png";
+import skill_dual_chakram_range from "@/assets/images/marineford/jango/jango-dual-chakram-range.png";
+import skill_one_two_jango_range from "@/assets/images/marineford/jango/jango-one-two-jango-range.png";
+//SKILL CAST
+import skill_stronger_cast from "@/assets/images/marineford/jango/jango-stronger-cast.mp4";
+import skill_kira_chakram_cast from "@/assets/images/marineford/jango/jango-kira-chakram-cast.mp4";
+import skill_dual_chakram_cast from "@/assets/images/marineford/jango/jango-dual-chakram-cast.mp4";
+import skill_one_two_jango_cast from "@/assets/images/marineford/jango/jango-one-two-jango-video.mp4";
+//SKILL VIDEO
+import skill_stronger_video from "@/assets/images/marineford/jango/jango-stronger-video.mp4";
+import skill_kira_chakram_video from "@/assets/images/marineford/jango/jango-kira-chakram-video.mp4";
+import skill_dual_chakram_video from "@/assets/images/marineford/jango/jango-dual-chakram-video.mp4";
+import skill_one_two_jango_video from "@/assets/images/marineford/jango/jango-one-two-jango-video.mp4";
+
 const Jango = () => {
   return (
     <>
@@ -26,24 +42,155 @@ const Jango = () => {
       <Title>Informacje:</Title>
       <Box>
         <List>
-          <ListItem>test</ListItem>
-          <ListItem>test</ListItem>
-          <ListItem>test</ListItem>
+          <ListItem>Jango na start przywołuje falę marinsów</ListItem>
+          <ListItem>
+            Następna fala marinsów pojawia się po 20 sekundach
+          </ListItem>
+          <ListItem>
+            Kolejne fale marinsów będą się pojawiać co 45 sekundach
+          </ListItem>
         </List>
       </Box>
       <Separator />
       <Title>Umiejętności:</Title>
       <Box>
-        <Text>1. Spell</Text>
-        <Tabs defaultValue="range" className="w-[400px]">
+        <Text>
+          1. Stronger! Stronger! - Jango buffuje marinsów dając im 25%
+          ataku i 40 MS
+        </Text>
+        <Tabs defaultValue="range" className="w-[1000px]">
           <TabsList>
             <TabsTrigger value="range">Range</TabsTrigger>
             <TabsTrigger value="cast">Cast</TabsTrigger>
-            <TabsTrigger value="gif">GIF</TabsTrigger>
+            <TabsTrigger value="video">Video</TabsTrigger>
           </TabsList>
-          <TabsContent value="range">Range</TabsContent>
-          <TabsContent value="cast">Cast</TabsContent>
-          <TabsContent value="gif">Gif</TabsContent>
+          <TabsContent value="range">
+            <img src={skill_stronger_range} alt="skill" />
+          </TabsContent>
+          <TabsContent value="cast">
+            <video
+              src={skill_stronger_cast}
+              loop
+              autoPlay
+              controls
+              muted
+            />
+          </TabsContent>
+          <TabsContent value="video">
+            <video
+              src={skill_stronger_video}
+              loop
+              autoPlay
+              controls
+              muted
+            />
+          </TabsContent>
+        </Tabs>
+      </Box>
+      <Separator />
+      <Box>
+        <Text>
+          2. Kira Chakram - Jango porusza się z czakramami wokół
+          siebie na obszarze 5x5
+        </Text>
+        <Tabs defaultValue="range" className="w-[1000px]">
+          <TabsList>
+            <TabsTrigger value="range">Range</TabsTrigger>
+            <TabsTrigger value="cast">Cast</TabsTrigger>
+            <TabsTrigger value="video">Video</TabsTrigger>
+          </TabsList>
+          <TabsContent value="range">
+            <img src={skill_kira_chakram_range} alt="skill" />
+          </TabsContent>
+          <TabsContent value="cast">
+            <video
+              src={skill_kira_chakram_cast}
+              loop
+              autoPlay
+              controls
+              muted
+            />
+          </TabsContent>
+          <TabsContent value="video">
+            <video
+              src={skill_kira_chakram_video}
+              loop
+              autoPlay
+              controls
+              muted
+            />
+          </TabsContent>
+        </Tabs>
+      </Box>
+      <Separator />
+      <Box>
+        <Text>
+          3. Dual Chakram - Skill anty-range ale może rzucić na
+          blisko. Jango puszcza chakram na obszarze 3x5
+        </Text>
+        <Tabs defaultValue="range" className="w-[1000px]">
+          <TabsList>
+            <TabsTrigger value="range">Range</TabsTrigger>
+            <TabsTrigger value="cast">Cast</TabsTrigger>
+            <TabsTrigger value="video">Video</TabsTrigger>
+          </TabsList>
+          <TabsContent value="range">
+            <img src={skill_dual_chakram_range} alt="skill" />
+          </TabsContent>
+          <TabsContent value="cast">
+            <video
+              src={skill_dual_chakram_cast}
+              loop
+              autoPlay
+              controls
+              muted
+            />
+          </TabsContent>
+          <TabsContent value="video">
+            <video
+              src={skill_dual_chakram_video}
+              loop
+              autoPlay
+              controls
+              muted
+            />
+          </TabsContent>
+        </Tabs>
+      </Box>
+      <Separator />
+      <Box>
+        <Text>
+          4. 1,2, Jango! - Jango zaczyna mówić nazwę swojej
+          umiejętnośći. Jeśli nikt nie zada mu obrażeń usypia
+          wszystkich na 6 sekund.
+        </Text>
+        <Tabs defaultValue="range" className="w-[1000px]">
+          <TabsList>
+            <TabsTrigger value="range">Range</TabsTrigger>
+            <TabsTrigger value="cast">Cast</TabsTrigger>
+            <TabsTrigger value="video">Video</TabsTrigger>
+          </TabsList>
+          <TabsContent value="range">
+            <img src={skill_one_two_jango_range} alt="skill" />
+          </TabsContent>
+          <TabsContent value="cast">
+            <video
+              src={skill_one_two_jango_cast}
+              loop
+              autoPlay
+              controls
+              muted
+            />
+          </TabsContent>
+          <TabsContent value="video">
+            <video
+              src={skill_one_two_jango_video}
+              loop
+              autoPlay
+              controls
+              muted
+            />
+          </TabsContent>
         </Tabs>
       </Box>
     </>
