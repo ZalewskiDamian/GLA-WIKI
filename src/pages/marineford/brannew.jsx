61f638133aab398a9e7ebdcb770d85e933626d81
ZalewskiDamian <damian.github@gmail.com>
2024-10-24 14:26:48 +0200

    add brannew skills range

--- src/pages/marineford/brannew.jsx
+++ src/pages/marineford/brannew.jsx
@@ -15,6 +15,14 @@ import { Text } from "@/components/ui/text";
 
 import headingImage from "@/assets/images/heading/brannew-heading.png";
 
+//SKILLS RANGE
+import skill_gatling_gun_range from "@/assets/images/marineford/brannew/brannew-gatling-gun-range.png";
+import skill_haki_punch_range from "@/assets/images/marineford/brannew/brannew-haki-punch-range.png";
+import skill_haki_stomp_range from "@/assets/images/marineford/brannew/brannew-haki-stomp-range.png";
+import skill_jump_range from "@/assets/images/marineford/brannew/brannew-jump-range.png";
+import skill_marine_hold_range from "@/assets/images/marineford/brannew/brannew-marine-hold-range.png";
+import skill_onigumo_range from "@/assets/images/marineford/brannew/brannew-onigumo-range.png";
+
 const Brannew = () => {
   return (
     <>
@@ -26,24 +34,123 @@ const Brannew = () => {
       <Title>Informacje:</Title>
       <Box>
         <List>
-          <ListItem>test</ListItem>
-          <ListItem>test</ListItem>
-          <ListItem>test</ListItem>
+          <ListItem>
+            Podczas bicia bossa należy trzymać się blisko siebie,
+            ponieważ może zaatakować marinsami którzy nas trzymają
+          </ListItem>
+          <ListItem>
+            Nie wchdzić pomiędzy osobę która jest atakowana Gataling
+            Gunem
+          </ListItem>
         </List>
       </Box>
       <Separator />
       <Title>Umiejętności:</Title>
       <Box>
-        <Text>1. Spell</Text>
-        <Tabs defaultValue="range" className="w-[400px]">
+        <Text>
+          1. Gatling Gun - Kiedy na postaci pojawia się celownik musi
+          ona uciekać slalomem od Brannewa. Obszar bicia jest między
+          celem a Brannewem, dlatego nie można wchodzić między
+          atakowaną osobę.
+        </Text>
+        <Tabs defaultValue="range" className="w-[1000px]">
           <TabsList>
             <TabsTrigger value="range">Range</TabsTrigger>
             <TabsTrigger value="cast">Cast</TabsTrigger>
-            <TabsTrigger value="gif">GIF</TabsTrigger>
+            <TabsTrigger value="video">Video</TabsTrigger>
           </TabsList>
-          <TabsContent value="range">Range</TabsContent>
+          <TabsContent value="range">
+            <img src={skill_gatling_gun_range} alt="skill" />
+          </TabsContent>
           <TabsContent value="cast">Cast</TabsContent>
-          <TabsContent value="gif">Gif</TabsContent>
+          <TabsContent value="video">Video</TabsContent>
+        </Tabs>
+      </Box>
+      <Separator />
+      <Box>
+        <Text>
+          2. Haki Punch & Jump - Atak sekwencyjn. Brannew uderza 2x
+          pięścią oraz skacze zadając obrażenia na obszarze 3x3 od
+          niego.
+        </Text>
+        <Tabs defaultValue="range" className="w-[1000px]">
+          <TabsList>
+            <TabsTrigger value="range">Range</TabsTrigger>
+            <TabsTrigger value="cast">Cast</TabsTrigger>
+            <TabsTrigger value="video">Video</TabsTrigger>
+          </TabsList>
+          <TabsContent value="range">
+            <img
+              src={skill_haki_punch_range}
+              alt="skill"
+              className="mb-2"
+            />
+            <img src={skill_haki_stomp_range} alt="skill" />
+          </TabsContent>
+          <TabsContent value="cast">Cast</TabsContent>
+          <TabsContent value="video">Video</TabsContent>
+        </Tabs>
+      </Box>
+      <Separator />
+      <Box>
+        <Text>
+          3. Jump - Brannew skacze do góry. Ląduje w miejscu w którym
+          jest cień zadając obrażenia na obszarze 3x3 od niego.
+        </Text>
+        <Tabs defaultValue="range" className="w-[1000px]">
+          <TabsList>
+            <TabsTrigger value="range">Range</TabsTrigger>
+            <TabsTrigger value="cast">Cast</TabsTrigger>
+            <TabsTrigger value="video">Video</TabsTrigger>
+          </TabsList>
+          <TabsContent value="range">
+            <img src={skill_jump_range} alt="skill" />
+          </TabsContent>
+          <TabsContent value="cast">Cast</TabsContent>
+          <TabsContent value="video">Video</TabsContent>
+        </Tabs>
+      </Box>
+      <Separator />
+      <Box>
+        <Text>
+          4. Marine Hold - Brannew przywołuje 3 marinsów którzy
+          trzymają postacie. Dlatego warto trzymać się obok siebie
+          żeby za pomocą obszarowych umiejętności uwolnić wszystkich
+          na raz. Jeśli się to nie uda należy poczekać aż na jednej z
+          postaci pojawi się celownik - ta osoba będzie atakowana
+          pierwsza. Postacie mele muszą uważać ponieważ obszar bicia
+          Gatling Guna bije też wokół celu.
+        </Text>
+        <Tabs defaultValue="range" className="w-[1000px]">
+          <TabsList>
+            <TabsTrigger value="range">Range</TabsTrigger>
+            <TabsTrigger value="cast">Cast</TabsTrigger>
+            <TabsTrigger value="video">Video</TabsTrigger>
+          </TabsList>
+          <TabsContent value="range">
+            <img src={skill_marine_hold_range} alt="skill" />
+          </TabsContent>
+          <TabsContent value="cast">Cast</TabsContent>
+          <TabsContent value="video">Video</TabsContent>
+        </Tabs>
+      </Box>
+      <Separator />
+      <Box>
+        <Text>
+          5. Onigumo - Brannew przywołuje Onigumo, który kręci się
+          dookoła niego. Należy odejść od Brannewa na 5 sqm.
+        </Text>
+        <Tabs defaultValue="range" className="w-[1000px]">
+          <TabsList>
+            <TabsTrigger value="range">Range</TabsTrigger>
+            <TabsTrigger value="cast">Cast</TabsTrigger>
+            <TabsTrigger value="video">Video</TabsTrigger>
+          </TabsList>
+          <TabsContent value="range">
+            <img src={skill_onigumo_range} alt="skill" />
+          </TabsContent>
+          <TabsContent value="cast">Cast</TabsContent>
+          <TabsContent value="video">Video</TabsContent>
         </Tabs>
       </Box>
     </>
